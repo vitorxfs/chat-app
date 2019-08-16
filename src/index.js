9cf87095dee21bdeaa89654aa7dd1eca1a234a59
vitorxfs <viisanches98@gmail.com>
2019-08-16 14:02:10 -0300

    Set up socket.io

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+server = http.createServer(app);
+const io = socketio(server);
 
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New websocket connection');
+    
+    socket.emit('message', 'Welcome!');
+
+    socket.broadcast.emit('message', 'A new user has joined!');
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');
+    });
+
+});
+
+server.listen(port, () => {
     console.log('Server is up on port ', port);
 });
